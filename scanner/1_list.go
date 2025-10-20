a1823dfd1281ad344efc377fac8a69ac0e330612
KeremDUZENLI <krmdznl97@gmail.com>
2025-10-20 16:30:44 +0200

    BACKUP -- Sort is fixed

--- scanner/1_list.go
+++ scanner/1_list.go
@@ -3,11 +3,13 @@ package scanner
 import (
 	"os"
 	"path/filepath"
+	"sort"
+	"strings"
 )
 
-func ListFolders(path string) ([]string, error) {
+func ListFolders(root string) ([]string, error) {
 	folders := make([]string, 0, 64)
-	stack := []string{path}
+	stack := []string{root}
 
 	for len(stack) > 0 {
 		dir := stack[len(stack)-1]
@@ -19,10 +21,18 @@ func ListFolders(path string) ([]string, error) {
 		if err != nil {
 			continue
 		}
-		for _, e := range entries {
-			if e.IsDir() {
-				stack = append(stack, filepath.Join(dir, e.Name()))
+
+		folderNames := make([]string, 0, len(entries))
+		for _, entry := range entries {
+			if entry.IsDir() {
+				folderNames = append(folderNames, entry.Name())
 			}
+		}
+
+		sortStrings(folderNames)
+
+		for i := len(folderNames) - 1; i >= 0; i-- {
+			stack = append(stack, filepath.Join(dir, folderNames[i]))
 		}
 	}
 
@@ -30,17 +40,54 @@ func ListFolders(path string) ([]string, error) {
 }
 
 func ListFiles(folders []string) ([]string, error) {
+	if len(folders) == 0 {
+		return nil, nil
+	}
+
 	files := []string{}
-	for _, dir := range folders {
-		entries, err := os.ReadDir(dir)
-		if err != nil {
-			continue
-		}
-		for _, e := range entries {
-			if !e.IsDir() {
-				files = append(files, filepath.Join(dir, e.Name()))
-			}
+	root := folders[0]
+
+	err := listFilesRecursive(root, &files)
+	return files, err
+}
+
+func listFilesRecursive(dir string, files *[]string) error {
+	entries, err := os.ReadDir(dir)
+	if err != nil {
+		return err
+	}
+
+	folders := []string{}
+	fileNames := []string{}
+
+	for _, e := range entries {
+		if e.IsDir() {
+			folders = append(folders, e.Name())
+		} else {
+			fileNames = append(fileNames, e.Name())
 		}
 	}
-	return files, nil
+
+	sortStrings(folders)
+	sortStrings(fileNames)
+
+	for _, folder := range folders {
+		subdir := filepath.Join(dir, folder)
+		if err := listFilesRecursive(subdir, files); err != nil {
+			continue
+		}
+	}
+	for _, name := range fileNames {
+		*files = append(*files, filepath.Join(dir, name))
+	}
+
+	return nil
+}
+
+func sortStrings(names []string) {
+	sort.Slice(names, func(i, j int) bool { return sortAToZ(names[i], names[j]) })
+}
+
+func sortAToZ(a, b string) bool {
+	return strings.ToLower(a) < strings.ToLower(b)
 }
